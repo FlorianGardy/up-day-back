3d5ca0824763043ec377cd754a3faf3f9302b1cd
Galvao Diniz Jonathan <jo.diniz@live.fr>
2019-06-12 10:23:29 +0200

    installing sequelize

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
-"use strict";
+`use strict`;
 
-const Hapi = require("@hapi/hapi");
-require("dotenv").config();
+const { start } = require("./server");
 
-const init = async () => {
-  const server = Hapi.server({
-    port: 3000,
-    host: "localhost"
-  });
-
-  await server.start();
-  console.log("Server running on %s", server.info.uri);
-};
-
-process.on("unhandledRejection", err => {
-  console.log(err);
-  process.exit(1);
-});
-
-init();
+start();
---
+++ server.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const Hapi = require("@hapi/hapi");
+const connect = require("./db/connect");
+
+const server = Hapi.server({
+  port: 3000,
+  host: "localhost"
+});
+
+exports.init = async () => {
+  const sequelize = require("./db/connect");
+  await sequelize.sync({ force: true });
+  await server.initialize();
+  return server;
+};
+
+exports.start = async () => {
+  const sequelize = require("./db/connect");
+  await sequelize.sync();
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (err) {
+    console.error("Unable to connect to the database:", err);
+  }
+  await server.start();
+  console.log(`Server running at: ${server.info.uri}`);
+  return server;
+};
+
+process.on("unhandledRejection", err => {
+  console.log(err);
+  process.exit(1);
+});
