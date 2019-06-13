2ff4f9f5bdd43f89f3d877d5d4d3a330c6143f98
Galvao Diniz Jonathan <jo.diniz@live.fr>
2019-06-13 11:33:39 +0200

    Add tests for post user route

--- test/users.js
+++ test/users.js
@@ -50,4 +50,43 @@ describe("# Users", () => {
       should(payload).match([user1, user2]);
     });
   });
+
+  describe("## POST /users", () => {
+    it("responds  with 500 when no datas are sent", async () => {
+      const res = await server.inject({
+        method: "POST",
+        url: "/users",
+        payload: {}
+      });
+      should(res.statusCode).equal(500);
+    });
+
+    it("responds with 200 and data when datas send", async () => {
+      const res = await server.inject({
+        method: "POST",
+        url: "/users",
+        payload: {
+          name: "Jiji",
+          password: "1234"
+        }
+      });
+      should(res.statusCode).equal(200);
+      should.exist(res.payload);
+    });
+
+    it("datas exist in db when sended", async () => {
+      const user = {
+        name: "Cocou",
+        password: "1234"
+      };
+      const res = await server.inject({
+        method: "POST",
+        url: "/users",
+        payload: user
+      });
+      should(res.statusCode).equal(200);
+      const payload = JSON.parse(res.payload);
+      should(payload).match(user);
+    });
+  });
 });
