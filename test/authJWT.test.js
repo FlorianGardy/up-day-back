f26c447c09fadab9efd3648105be299b3dde5911
Florian Gardy <florian.gardy@gmail.com>
2019-07-02 14:06:14 +0200

    Reworked events and users tests. Created authJWT test.

---
+++ test/authJWT.test.js
@@ -0,0 +1,72 @@
+const should = require("should");
+const Hapi = require("@hapi/hapi");
+const { AUTH_JWT } = require("../plugins/authJwt");
+
+describe("# Auth JWT pluggin", () => {
+  let server;
+
+  beforeEach(async () => {
+    server = Hapi.server({
+      port: 3030,
+      host: "localhost",
+      routes: {
+        cors: true
+      }
+    });
+  });
+
+  it("should responds with 400 when headers is missing", async () => {
+    await server.register({
+      plugin: require("../plugins/authJwt")
+    });
+
+    server.route({
+      method: "GET",
+      path: "/random",
+      options: {
+        auth: AUTH_JWT
+      },
+      handler: function() {
+        return "hello";
+      }
+    });
+
+    const res = await server.inject({
+      method: "GET",
+      url: "/random"
+    });
+    should(res.statusCode).equal(400);
+  });
+
+  it("should responds with 200 and the handler has an access to req.auth.credentials (JWToken & uuid) when the header.athorization is set", async () => {
+    await server.register({
+      plugin: require("../plugins/authJwt"),
+      options: {
+        validate: () => "myUuid"
+      }
+    });
+
+    server.route({
+      method: "GET",
+      path: "/random",
+      options: {
+        auth: AUTH_JWT
+      },
+      handler: function(req) {
+        should(req.auth.credentials.JWToken).equal("coucou");
+        should(req.auth.credentials.uuid).equal("myUuid");
+        return "yo";
+      }
+    });
+
+    const res = await server.inject({
+      method: "GET",
+      url: "/random",
+      headers: {
+        authorization: "coucou"
+      }
+    });
+    should(res.statusCode).equal(200);
+    should(res.result).equal("yo");
+  });
+});
--- test/users.test.js
+++ test/users.test.js
@@ -14,7 +14,7 @@ describe("# Users", () => {
   });
 
   describe("## GET /users", () => {
-    it("responds with 200", async () => {
+    it("should responds with 200", async () => {
       const res = await server.inject({
         method: "GET",
         url: "/users"
@@ -22,7 +22,7 @@ describe("# Users", () => {
       should(res.statusCode).equal(200);
     });
 
-    it("responds an empty user list", async () => {
+    it("should responds an empty array if no user has been created yet", async () => {
       const res = await server.inject({
         method: "GET",
         url: "/users"
@@ -31,21 +31,27 @@ describe("# Users", () => {
       should(payload).deepEqual([]);
     });
 
-    it("responds an user list with two users", async () => {
+    it("should returns an array containing 2 users if 2 users exist in the database", async () => {
       const user1 = {
+        uuid: "1753df50-9cbf-11e9-bf9b-6da555a5236b",
         name: "Jojo",
         password: "1234",
-        role: "Admin",
-        token: "secret"
+        email: "jojo@gmail.com",
+        role: "admin",
+        token: "myToken1"
       };
       const user2 = {
+        uuid: "1753df50-9cbf-11e9-bf9b-6da555a5236c",
         name: "Floflo",
         password: "1234",
-        role: "Admin",
-        token: "secret"
+        email: "floflo@gmail.com",
+        role: "standard",
+        token: "myToken2"
       };
+
       await User.create(user1);
       await User.create(user2);
+
       const res = await server.inject({
         method: "GET",
         url: "/users"
@@ -72,29 +78,12 @@ describe("# Users", () => {
         payload: {
           name: "Jiji",
           password: "1234",
-          role: "Admin",
+          role: "admin",
           token: "secret"
         }
       });
       should(res.statusCode).equal(200);
       should.exist(res.payload);
     });
-
-    // it("datas exist in db when sended", async () => {
-    //   const user = {
-    //     name: "Cocou",
-    //     password: "1234",
-    //     role: "Admin",
-    //     token: "secret"
-    //   };
-    //   const res = await server.inject({
-    //     method: "POST",
-    //     url: "/users",
-    //     payload: user
-    //   });
-    //   should(res.statusCode).equal(200);
-    //   const payload = JSON.parse(res.payload);
-    //   should(payload).match(user);
-    // });
   });
 });
